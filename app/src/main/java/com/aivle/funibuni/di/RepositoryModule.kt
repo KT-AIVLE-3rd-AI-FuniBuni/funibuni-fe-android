4837033d6f6715775e3b914a57af1ffb7366557b
kt-loggi <jsl663663@gmail.com>
2023-06-03 14:29:37 +0900

    feat(app): add DI Module

---
+++ app/src/main/java/com/aivle/funibuni/di/RepositoryModule.kt
@@ -0,0 +1,10 @@
+package com.aivle.funibuni.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+}
---
+++ app/src/main/java/com/aivle/funibuni/di/UseCaseModule.kt
@@ -0,0 +1,10 @@
+package com.aivle.funibuni.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object UseCaseModule {
+}
